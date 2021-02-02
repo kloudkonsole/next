0c7a57c3459ef5383862ac9d4255330f27afebda
Darren Liew <ldarren@gmail.com>
2021-02-03 07:31:27 +0800

    fixed lint error

--- mod/it.js
+++ mod/it.js
@@ -1,14 +1,13 @@
 const pico = require('pico-common/bin/pico-cli')
 const {series, parallel} = pico.export('pico/test')
-const test = pico.export('pico/test')
 
 module.exports = {
 	setup(host, cfg, rsc, paths){
 	},
-	async parallel(desc, func){
+	parallel(desc, func){
 		parallel(desc, func(this))
 	},
-	async series(desc, func){
+	series(desc, func){
 		series(desc, func(this))
 	}
 }
